5ee95b86dc6cac654db7d746af04b1308cd041ad
KonoLv1 <konolevel1@gmail.com>
2019-10-05 18:56:21 +0900

    応用情報技術者試験用プログラム

--- Study/Day1/AP/AP.c
+++ Study/Day1/AP/AP.c
@@ -25,70 +25,64 @@ int main(){
   char buf4[BUF];
   char buf5[BUF];
   char buf6[BUF];
-
-
+  //  乱数初期化
+  srand((unsigned int)time(NULL));
+  //  問題用csvファイル読み込み 
   fp = fopen( fname, "r" );
   if( fp == NULL ){
     printf( "%sファイルが見つかりません\n", fname );
     return -1;
   }
-
- srand((unsigned int)time(NULL));
-
-
   fscanf(fp,"%s",buf);
+  // 問題数を読み込み
   max = atoi(buf);
-  for(i = 0; i < max; i++){
-
-  mode = rand() % 4 + 1;
-    
-  fscanf(fp, "%[^,],%[^,],%[^,],%[^,],%[^,],%s", buf1, buf2, buf3, buf4, buf5, buf6);
-  switch (mode)
-  {
-  case 1:
-    printf("%s\n\n 1: %s\n 2: %s\n 3: %s\n 4: %s\n ",buf1, buf2, buf3, buf4, buf5);
-    break;
-  case 2:
-    printf("%s\n\n 1: %s\n 2: %s\n 3: %s\n 4: %s\n ",buf1, buf5, buf2, buf3, buf4);
-    break;
-  case 3:
-    printf("%s\n\n 1: %s\n 2: %s\n 3: %s\n 4: %s\n ",buf1, buf4, buf5, buf2, buf3);
-    break;
-  case 4:
-    printf("%s\n\n 1: %s\n 2: %s\n 3: %s\n 4: %s\n ",buf1, buf3, buf4, buf5, buf2);
-    break;
-  default:
-    break;
+  // 問題数分ループ
+  for(i = 0; i < max; i++){  
+    fscanf(fp, "%[^,],%[^,],%[^,],%[^,],%[^,],%s", buf1, buf2, buf3, buf4, buf5, buf6);
+    // 選択肢をランダムで表示
+    mode = rand() % 4 + 1;
+    switch (mode){
+      case 1:
+        printf("%s\n\n 1: %s\n 2: %s\n 3: %s\n 4: %s\n ",buf1, buf2, buf3, buf4, buf5);
+        break;
+      case 2:
+        printf("%s\n\n 1: %s\n 2: %s\n 3: %s\n 4: %s\n ",buf1, buf5, buf2, buf3, buf4);
+        break;
+      case 3:
+        printf("%s\n\n 1: %s\n 2: %s\n 3: %s\n 4: %s\n ",buf1, buf4, buf5, buf2, buf3);
+        break;
+      case 4:
+        printf("%s\n\n 1: %s\n 2: %s\n 3: %s\n 4: %s\n ",buf1, buf3, buf4, buf5, buf2);
+        break;
+      default:
+        break;
+    }
+    printf("\n");
+    // 正解数と解いた問題数を加算
+    score = score + AnsewrCheck(buf6,score,all,mode);
+    all++;
   }
-
- 
-  printf("\n");
-  score = score + AnsewrCheck(buf6,score,all,mode);
-  all++;
-  }
-
-
-
-
   fclose( fp );
 }
 
-
+// 正誤判定とコマンド入力用関数
 int AnsewrCheck(char *buf6,int score,int all,int mode){
-  char b[256];
+  char user_ans[256];
   int buf7;
   char cmd1[] = "?";
   char cmd2[] = "SCORE";
+  // 正解か不正解を判定するまではループ
   while(1){
-
     //  解答、もしくはコマンド入力
-    scanf(" %s",&b);
+    scanf(" %s",&user_ans);
 
     //  コマンド判定　ここから
-    if(strcmp(b,cmd1)==0){
+    // ?が入力された場合
+    if(strcmp(user_ans,cmd1)==0){
       printf("--- INFORMATION ---\n ?     : INFORMATION\n SCORE : SCORE\n");
       continue;
-    }else if(strcmp(b,cmd2)==0){
+    //  SCOREが入力された場合
+    }else if(strcmp(user_ans,cmd2)==0){
       printf("\n SCORE: OK %d NG %d\n",score,all - score);
       continue;
     }
@@ -99,7 +93,7 @@ int AnsewrCheck(char *buf6,int score,int all,int mode){
       buf7 = buf7 - 4;
     }
     sprintf(buf6, "%d", buf7);
-    if(strcmp(buf6,b)==0){
+    if(strcmp(buf6,user_ans)==0){
       printf("OK\n\n");
       score = 1;
       break;
@@ -108,6 +102,7 @@ int AnsewrCheck(char *buf6,int score,int all,int mode){
       break;
     }
   }
+  // 正解なら1を、不正解なら0を返す
   return score;
 }
 
