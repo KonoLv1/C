4869054c2fa2771fece704489893984340899f9a
Konolv1 <konolevel1@gmail.com>
2019-10-07 11:26:53 +0900

    応用情報技術者試験対策

---
+++ Study/Day1/AtCoder/BeginnerContest/138/C/Red_or_Not.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+ 
+int main(void){
+
+	int a;
+	char s[10];
+ 
+    //  “ü—Íˆ—
+    
+    printf("2800ˆÈãA5000–¢–ž‚Ì®”‚ð“ü—Í‚µ‚Ä‚­‚¾‚³‚¢\n");
+    scanf("%d",&a);
+    printf("10•¶ŽšˆÈ‰º‚Ì•¶Žš—ñ‚ð“ü—Í‚µ‚Ä‚­‚¾‚³‚¢\n");
+    scanf("%s",s);
+ 
+    //  ŒvŽZˆ— o—Íˆ—
+    
+    if (a >= 3200){
+        printf("%s",s);
+    }else{
+        printf("red");
+    }
+
+}
