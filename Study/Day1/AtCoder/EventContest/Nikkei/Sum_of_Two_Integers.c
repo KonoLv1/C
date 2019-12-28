cc9c19ab5f9feba886093ccce9c5ad8ee53d3913
KonoLv1 <konolevel1@gmail.com>
2019-12-28 23:42:05 +0900

    AtCoder

--- Study/Day1/AtCoder/EventContest/Nikkei/Sum_of_Two_Integers.c
+++ Study/Day1/AtCoder/EventContest/Nikkei/Sum_of_Two_Integers.c
@@ -4,7 +4,7 @@
 
 int main(void){
     int n,count;
- 
+    comment();
     //  入力処理
     
     scanf("%d",&n);
@@ -20,4 +20,14 @@ int main(void){
     // 出力処理
     printf("%d",count);
   
+}
+
+void comment(void){
+    printf("任意の数値を入力してください。");
+}
+
+void endcheck(void){
+    char end[256];
+    printf("\n\n終了するには数字を入力してください");
+    scanf("%s",end);
 }
